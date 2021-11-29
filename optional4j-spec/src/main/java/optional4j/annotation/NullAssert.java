1a69b11709aa1c577a3724517fce01c4bf2f4299
amarees <saleh.amareen@dominos.com>
2021-11-29 12:50:18 -0500

    LocalVariable target

--- optional4j-spec/src/main/java/optional4j/annotation/NullAssert.java
+++ optional4j-spec/src/main/java/optional4j/annotation/NullAssert.java
@@ -1,3 +1,8 @@
 package optional4j.annotation;
 
+import static java.lang.annotation.ElementType.*;
+
+import java.lang.annotation.Target;
+
+@Target(LOCAL_VARIABLE)
 public @interface NullAssert {}
--- optional4j-spec/src/main/java/optional4j/annotation/NullSafe.java
+++ optional4j-spec/src/main/java/optional4j/annotation/NullSafe.java
@@ -1,3 +1,8 @@
 package optional4j.annotation;
 
+import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
+
+import java.lang.annotation.Target;
+
+@Target(LOCAL_VARIABLE)
 public @interface NullSafe {}
